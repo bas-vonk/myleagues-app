f389f4c20ae12261a570f45a21ded3e078c5515a
Bas Vonk <bas.vonk@pacmed.nl>
2021-10-12 22:24:00 +0200

    Add gtag for Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 
 import { router } from "./router";
 import { store } from "./store";
@@ -19,5 +20,9 @@ const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.use(VueGtag, {
+  config: { id: "G-M3655P6W7N", params: { send_page_view: false } },
+});
 
 app.mount("#app");
