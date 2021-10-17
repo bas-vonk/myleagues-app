16c86d67b610f9599c4cfd777a13a3f3ef3da990
Bas Vonk <bas.vonk@pacmed.nl>
2021-10-17 21:27:25 +0200

    #11 Put login form in a separate component.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,56 +1,29 @@
 <template>
   <div class="container">
     <div class="h1" id="appName">MYLEAGUES</div>
-    <form v-on:submit.prevent="submitForm">
-      <div class="form-group">
-        <input
-          type="email"
-          class="form-control"
-          placeholder="Email address"
-          v-model="emailAddress"
-        />
-      </div>
-      <div class="form-group">
-        <input
-          type="password"
-          class="form-control"
-          placeholder="Password"
-          v-model="password"
-        />
-      </div>
-      <div class="form-group" v-if="showRegisterFields">
-        <input
-          type="password"
-          class="form-control"
-          placeholder="Password confirmation"
-          v-model="passwordConfirmation"
-        />
-      </div>
-      <div class="form-group" v-if="showRegisterFields">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Username"
-          v-model="username"
-        />
-      </div>
-      <div class="alert alert-warning" v-if="isError">
-        {{ errorMessage }}
-      </div>
-      <button type="submit" class="btn btn-primary">
-        {{ buttonText }}
-      </button>
-    </form>
-    <small id="modeSwitchText" @click="toggleMode()">{{
-      modeSwitchText
-    }}</small>
+    <login-register-form
+      :isError="isError"
+      :errorMessage="errorMessage"
+      @submit="submitForm"
+    ></login-register-form>
   </div>
 </template>
 
 <script>
+import LoginRegisterForm from "@/components/forms/LoginRegisterForm.vue";
+
 import { store } from "@/store";
 
 export default {
+  components: {
+    LoginRegisterForm,
+  },
+  data() {
+    return {
+      isError: false,
+      errorMessage: null,
+    };
+  },
   beforeRouteEnter(to, from, next) {
     if (store.getters["user/isLoggedIn"]) {
       next({ name: "home" });
@@ -58,35 +31,19 @@ export default {
       next();
     }
   },
-  data() {
-    return {
-      mode: "login",
-      emailAddress: null,
-      password: null,
-      passwordConfirmation: null,
-      username: null,
-      isError: false,
-      errorMessage: null,
-    };
-  },
   methods: {
-    toggleMode() {
-      this.mode = this.mode !== "login" ? "login" : "register";
-      this.isError = false;
-      this.errorMessage = null;
-    },
     navigateToHome() {
       this.$router.push({ name: "home" });
     },
-    async login() {
+    async login(formData) {
       try {
         // Start spinner
         this.$store.dispatch("setIsGlobalLoading", true, { root: true });
 
         // Dispatch the login action
         await this.$store.dispatch("user/Login", {
-          emailAddress: this.emailAddress,
-          password: this.password,
+          emailAddress: formData.emailAddress,
+          password: formData.password,
         });
       } catch (error) {
         this.isError = true;
@@ -112,8 +69,8 @@ export default {
         this.$store.dispatch("setIsGlobalLoading", false, { root: true });
       }
     },
-    async register() {
-      if (!this.passwordsMatch) {
+    async register(formData) {
+      if (!this.passwordsMatch(formData)) {
         throw "Passwords don't match.";
       }
 
@@ -123,9 +80,9 @@ export default {
 
         // Register the user and go to the home page.
         await this.$store.dispatch("user/Register", {
-          emailAddress: this.emailAddress,
-          password: this.password,
-          username: this.username,
+          emailAddress: formData.emailAddress,
+          password: formData.password,
+          username: formData.username,
         });
         this.navigateToHome();
       } catch (error) {
@@ -136,46 +93,26 @@ export default {
         this.$store.dispatch("setIsGlobalLoading", false, { root: true });
       }
     },
-    async submitForm() {
-      if (this.mode === "login") {
-        await this.login();
+    async submitForm(formData) {
+      if (formData.mode === "login") {
+        await this.login(formData);
       } else {
-        await this.register();
+        await this.register(formData);
       }
     },
-  },
-  computed: {
-    buttonText() {
-      return this.mode === "login" ? "Login" : "Register";
-    },
-    modeSwitchText() {
-      return this.mode === "login" ? "Register instead." : "Login instead.";
-    },
-    showRegisterFields() {
-      return this.mode === "register";
-    },
-    passwordsMatch() {
-      return this.password === this.passwordConfirmation;
+    passwordsMatch(formData) {
+      return formData.password === formData.passwordConfirmation;
     },
   },
 };
 </script>
 
 <style lang="css" scoped>
-.alert {
-  width: 100%;
-}
 .container {
   padding-top: 20%;
   width: 18rem;
   padding: auto;
   margin: auto;
-}
-.form-group {
-  width: 100%;
-  height: 100%;
-  margin: auto;
-  padding: auto;
 }
 #appName {
   font-size: 2rem;
@@ -183,21 +120,5 @@ export default {
   color: white;
   font-weight: 250;
   letter-spacing: 0.15em;
-}
-#modeSwitchText {
-  font-size: 1rem;
-  font-weight: 500;
-  cursor: pointer;
-  color: white;
-  text-shadow: 1px 1px #000000;
-}
-button,
-input {
-  margin: 0.5rem auto;
-  width: 100%;
-  border-style: none;
-}
-button {
-  background-color: #f79e02;
 }
 </style>
