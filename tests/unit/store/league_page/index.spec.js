466b735bb58764cc22d3765994a1250f0b45a908
Bas Vonk <bas.vonk@pacmed.nl>
2021-10-22 08:58:44 +0200

    Right path forward with the unit tests

--- tests/unit/store/league_page/index.spec.js
+++ tests/unit/store/league_page/index.spec.js
@@ -4,37 +4,43 @@ import { store } from "@/store";
 import { leagueResponse } from "./league";
 import moxios from "moxios";
 
-function respondWith(response) {
-  return new Promise((resolve, reject) => {
-    moxios.wait(() => {
-      moxios.requests
-        .mostRecent()
-        .respondWith({
-          status: 200,
-          response,
-        })
-        .then(resolve, reject);
-    });
-  });
-}
+const flushPromises = require("flush-promises");
 
-describe("@/store/league_page/index.js", () => {
+describe("Store: league page.", () => {
   // Mock the API
   beforeEach(() => moxios.install(Api));
   afterEach(() => moxios.uninstall(Api));
 
-  it("Tests league page module (actions).", async () => {
+  it("Shall call the API to get a league and store the results.", async () => {
     let leagueId = 1;
+
+    moxios.stubRequest(`league/${leagueId}`, {
+      status: 200,
+      response: leagueResponse,
+    });
 
     store.dispatch("league_page/GetForIdAndStore", leagueId);
 
-    await respondWith(leagueResponse);
+    await flushPromises();
 
     expect(store.getters["league_page/joinCode"]).toBe(
       leagueResponse.data.attributes.join_code
     );
     expect(store.getters["league_page/players"]).toStrictEqual(
       leagueResponse.data.attributes.players
+    );
+    expect(store.getters["league_page/matches"]).toStrictEqual(
+      leagueResponse.data.attributes.matches
+    );
+    expect(store.getters["league_page/ranking"]).toStrictEqual(
+      leagueResponse.data.attributes.ranking
+    );
+    expect(store.getters["league_page/name"]).toBe(
+      leagueResponse.data.attributes.name
+    );
+    expect(store.getters["league_page/leagueId"]).toBe(leagueResponse.data.id);
+    expect(store.getters["league_page/rankingSystem"]).toBe(
+      leagueResponse.data.attributes.ranking_system
     );
   });
 });
