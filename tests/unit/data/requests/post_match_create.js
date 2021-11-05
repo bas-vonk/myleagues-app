d691c39edc09fa691190c2e535c248ba29aa4213
Bas Vonk <bas.vonk@pacmed.nl>
2021-11-05 15:58:05 +0100

    Added coverage for the submit match result card

---
+++ tests/unit/data/requests/post_match_create.js
@@ -0,0 +1,8 @@
+export const matchCreateRequest = {
+  league_id: "ffde1b13-5c83-4f32-a57d-3165e4d96101",
+  home_player_id: "0c02262a-a688-47cb-b5a0-c4f6099648c5",
+  home_score: 1,
+  away_player_id: "697b38db-2e54-40e3-a4fb-82617c7a3a1b",
+  away_score: 2,
+  date: "2021-01-05",
+};
