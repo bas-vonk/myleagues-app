7428e041142595fbd77715ecc5b06135218cb1b9
Bas Vonk <bas.vonk@pacmed.nl>
2021-11-08 09:35:50 +0100

    Covered matches table

---
+++ tests/unit/components/tables/MatchesTable.spec.js
@@ -0,0 +1,43 @@
+import { mount } from "@vue/test-utils";
+import MatchesTable from "@/components/tables/MatchesTable.vue";
+
+describe("MatchesTable.vue", () => {
+  it("Shall render a table with all matches.", () => {
+    const gmt_timestamp = "Sun, 31 Oct 2021 23:00:00 GMT";
+    const displayed_timestamp = "2021-11-01";
+    const matches = [
+      {
+        date: gmt_timestamp,
+        home_player_username: "home",
+        home_score: 1,
+        away_player_username: "away",
+        away_score: 2,
+      },
+      {
+        date: gmt_timestamp,
+        home_player_username: "home",
+        home_score: 1,
+        away_player_username: "away",
+        away_score: 2,
+      },
+      {
+        date: gmt_timestamp,
+        home_player_username: "home",
+        home_score: 1,
+        away_player_username: "away",
+        away_score: 2,
+      },
+    ];
+
+    var wrapper = mount(MatchesTable, {
+      props: { matches },
+    });
+
+    // Assert that the date is properly formatted and
+    // that the winning side is displayed bold
+    let tableDataElements = wrapper.findAll("td");
+    expect(tableDataElements.at(0).text()).toContain(displayed_timestamp);
+    expect(tableDataElements.at(4).classes()).toContain("fw-bold");
+    expect(tableDataElements.at(5).classes()).toContain("fw-bold");
+  });
+});
