941e892b0c8ceab22fab2dbe5b93fefbc7d50a2f
Bas Vonk <sjj.vonk@gmail.com>
2021-12-29 19:39:06 +0100

    Fix issue with default month when adding a result. (#27)

--- src/components/forms/SubmitMatchResultForm.vue
+++ src/components/forms/SubmitMatchResultForm.vue
@@ -70,7 +70,7 @@ var today = new Date();
 var dateDefaultValue =
   today.getFullYear() +
   "-" +
-  ("0" + today.getMonth() + 1).slice(-2) +
+  ("0" + (today.getMonth() + 1)).slice(-2) +
   "-" +
   ("0" + today.getDate()).slice(-2);
 
